28acbbfd5ded1d5b816f46566a3bd25e1af53c45
Sidhu, Gautam (DevOps Engineer) <gautam.sidhu@sky.uk>
2017-06-30 12:59:21 +0100

    Initial commit

---
+++ cmd/gitUrl.go
@@ -0,0 +1,46 @@
+// Copyright © 2017 NAME HERE <EMAIL ADDRESS>
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// gitUrlCmd represents the gitUrl command
+var gitUrlCmd = &cobra.Command{
+	Use:   "gitUrl",
+	Short: "remote repo url",
+	Long: `Checks out remote git repo`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("gitUrl called")
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(gitUrlCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	gitUrlCmd.PersistentFlags().String("url", "", "git remote url")
+
+	gitUrlCmd.PersistentFlags().String("tag", "", "tag to use")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// gitUrlCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
