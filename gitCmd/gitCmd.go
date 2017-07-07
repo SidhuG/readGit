ed327dda80dd58641b0a96522454f6d17f14eba5
Sidhu, Gautam (DevOps Engineer) <gautam.sidhu@sky.uk>
2017-07-07 17:25:43 +0100

    added some comments

--- gitCmd/gitCmd.go
+++ gitCmd/gitCmd.go
@@ -10,11 +10,20 @@ import (
 	"fmt"
 	"os"
 	"gopkg.in/libgit2/git2go.v25"
+	homedir "github.com/mitchellh/go-homedir"
+	"path/filepath"
 	//"strconv"
 	//"reflect"
 )
 
-func CheckOutRepo(giturl string, gittag string, sshid string) (dirPath string, err error){
+func CheckOutRepo(giturl string, gitbranch string, gittag string, sshid string, ) (dirPath string, err error){
 	
+	//1. Make tmp dir
+	//2. Clean tmp directory if needed
+	
+	//3. git clone
+
+	//4. return the path to where repo has been cloned
+
 
 }
